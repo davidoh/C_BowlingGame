1b0d08b3889522db7f582a4688023213fc1ad072
David Oh <david.siang.fong.oh@gmail.com>
2014-04-13 22:56:33 -0400

    doh - wrote first passing unit test

---
+++ bowling_game.c
@@ -0,0 +1,15 @@
+#include "bowling_game.h"
+
+static int score;
+
+void bowling_game_init() {
+  score = 0;
+}
+
+void bowling_game_roll(int pins) {
+
+}
+
+int bowling_game_score() {
+  return score;
+}
--- bowling_game_test.c
+++ bowling_game_test.c
@@ -1,6 +1,16 @@
+#include "bowling_game.h"
 #include <assert.h>
 #include <stdbool.h>
 
+static void test_gutter_game() {
+  bowling_game_init();
+  for (int i=0; i<20; i++) {
+    bowling_game_roll(0);
+  }
+  assert( bowling_game_score() == 0 && "test_gutter_game()" );
+}
+
+
 int main() {
-    assert( false && "My first unit test");
+  test_gutter_game();
 }
