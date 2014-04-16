f9931dbb3ace1fa5559d8dc574531e8e51bf2357
David Oh <david.siang.fong.oh@gmail.com>
2014-04-15 23:47:02 -0400

    doh - added functionality for spares

--- src/bowling_game.c
+++ src/bowling_game.c
@@ -1,15 +1,31 @@
 #include "bowling_game.h"
 
-static int score;
+enum { max_rolls = 21, max_frames = 10 };
+static int rolls[max_rolls];
+static int current_roll;
 
 void bowling_game_init() {
-  score = 0;
+  for(int i=0; i< max_rolls; i++) {
+    rolls[i] = 0;
+  }
+  current_roll = 0;
 }
 
 void bowling_game_roll(int pins) {
-  score += pins;
+  rolls[current_roll++] = pins;
 }
 
 int bowling_game_score() {
+  int score = 0;
+  int i = 0;
+  for(int frame=0; frame < max_frames; frame++) {
+    int frame_score = rolls[i] + rolls[i+1];
+    score += frame_score;
+    if(frame_score == 10) {
+      int next_roll = rolls[i+2];
+      score += next_roll;
+    }
+    i += 2;
+  }
   return score;
 }
--- tests/bowling_game_test.c
+++ tests/bowling_game_test.c
@@ -11,16 +11,26 @@ static void roll_many(int rolls, int pins) {
 static void test_gutter_game() {
   bowling_game_init();
   roll_many(20, 0);
-  assert( bowling_game_score() == 0 && "test_gutter_game()" );
+  assert( bowling_game_score() == 0);
 }
 
 static void test_all_ones() {
   bowling_game_init();
   roll_many(20, 1);
-  assert( bowling_game_score() == 20 && "test_gutter_game()" );
+  assert( bowling_game_score() == 20);
+}
+
+static void test_one_spare() {
+  bowling_game_init();
+  bowling_game_roll(5);
+  bowling_game_roll(5);
+  bowling_game_roll(3);
+  roll_many(17, 0);
+  assert( bowling_game_score() == 16);
 }
 
 int main() {
   test_gutter_game();
   test_all_ones();
+  test_one_spare();
 }
