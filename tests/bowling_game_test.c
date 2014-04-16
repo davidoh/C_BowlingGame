7001d4ee712c2920db689a9bca615b2489ba36ae
David Oh <david.siang.fong.oh@gmail.com>
2014-04-15 23:15:25 -0400

    doh - added test for non-zero rolls

--- tests/bowling_game_test.c
+++ tests/bowling_game_test.c
@@ -2,15 +2,25 @@
 #include <assert.h>
 #include <stdbool.h>
 
+static void roll_many(int rolls, int pins) {
+  for (int i=0; i<rolls; i++) {
+    bowling_game_roll(pins);
+  }
+}
+
 static void test_gutter_game() {
   bowling_game_init();
-  for (int i=0; i<20; i++) {
-    bowling_game_roll(0);
-  }
+  roll_many(20, 0);
   assert( bowling_game_score() == 0 && "test_gutter_game()" );
 }
 
+static void test_all_ones() {
+  bowling_game_init();
+  roll_many(20, 1);
+  assert( bowling_game_score() == 20 && "test_gutter_game()" );
+}
 
 int main() {
   test_gutter_game();
+  test_all_ones();
 }
